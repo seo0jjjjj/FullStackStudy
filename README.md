d4d9eb3ca6f480a8fee1ac1a7c05c2f3f664d7e6
moble.yunoh <seo0jjjjj@mail.com>
2024-05-08 15:23:56 +0900

    refactor: 필요없는 테스트 코드 및 스타일 파일 삭제

---
+++ README.md
@@ -0,0 +1,14 @@
+# 리엑트 공부용 프로젝트
+<table>
+  <th>
+  <td>Front-End</td>
+  <td colspan=2>Back-End</td>
+  </th>
+  <tr>
+    <td>React</td>
+    <td>Express</td>
+    <td>mongoDB</td>
+  </tr>
+
+</table>
+
---
+++ backend/server.js
@@ -0,0 +1,6 @@
+require('dotenv').config({path: "./keys.env"});
+
+console.log(process.env.DB_HOST);	// 127.0.0.1
+console.log(process.env.DB_PORT);	// 3306
+console.log(process.env.DB_USER);	// root
+console.log(process.env.DB_PASSWORD);	// 0000
