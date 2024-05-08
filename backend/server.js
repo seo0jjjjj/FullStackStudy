ef76eb08ccc437d423dcfadf4f4da98670469e90
moble.yunoh <seo0jjjjj@mail.com>
2024-05-08 17:22:21 +0900

    feat: add onChange to EditForm

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,5 @@ console.log(process.env.DB_HOST);	// 127.0.0.1
 console.log(process.env.DB_PORT);	// 3306
 console.log(process.env.DB_USER);	// root
 console.log(process.env.DB_PASSWORD);	// 0000
+
+
--- frontend/src/components/TodoComponents.js
+++ frontend/src/components/TodoComponents.js
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 import '../style/todoStyle.css'
-import 'boxicons'
+import { useState } from "react";
 
 export function TodoNav(props){
   return(
@@ -74,11 +74,15 @@ export function CreateTodo(props){
 
 function TodoForm(props){
   const {title, btnText, onSubmit, todoContent} = props;
+  const [todoText, setTodoText] = useState(todoContent);
+  const onTextChange = (e)=>{
+    setTodoText(e.target.value);
+  }
 
   return(<>
     <fieldset>
       <legend>{title ?? "제목"}</legend>
-      <input type="text" id="content" placeholder="할일을 입력하세요." value={todoContent ?? ""}/>
+      <input type="text" id="content" placeholder="할일을 입력하세요." value={todoText ?? ""} onChange={onTextChange}/>
       <button onClick={onSubmit} className="btn">{btnText ?? "버튼"}</button>
     </fieldset>
   </>)
