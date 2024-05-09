39df4a208c9d01b639ee3e91ffcb859439bdba92
moble.yunoh <seo0jjjjj@mail.com>
2024-05-09 13:51:53 +0900

    fix: 오류해결

--- frontend/src/page/Todo.js
+++ frontend/src/page/Todo.js
@@ -1,6 +1,7 @@
 import { Navigate, Outlet, useOutlet } from "react-router-dom";
 import { Header } from "../components/Header";
 import { TodoNav } from "../components/TodoNav";
+import '../style/todoStyle.css'
 
 export function Todo(props) {
   const hasOulet = useOutlet();
