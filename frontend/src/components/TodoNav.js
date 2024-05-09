ffa45ee94de3e43b6ba0967c8ecaa9da348f857d
moble.yunoh <seo0jjjjj@mail.com>
2024-05-09 14:01:42 +0900

    style: 상단 nav바 수정

--- frontend/src/components/TodoNav.js
+++ frontend/src/components/TodoNav.js
@@ -6,6 +6,6 @@ export function TodoNav(props) {
       <NavLink to="/add" className="nav-item">할일 추가</NavLink>
       <NavLink to="/list" className="nav-item">할일 보기</NavLink>
       {/* <NavLink to="/delete" className="nav-item">할일 삭제</NavLink> */}
-      <NavLink to="/edit" className="nav-item">할일 수정</NavLink>
+      {/* <NavLink to="/edit" className="nav-item">할일 수정</NavLink> */}
     </ol>);
 }
