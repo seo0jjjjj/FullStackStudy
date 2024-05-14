b556b839b18fd7dd4d56c733c19cd4b501a15a81
moble.yunoh <seo0jjjjj@mail.com>
2024-05-14 14:37:04 +0900

    refactor: 로그인 h1 -> header로 수정

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,8 +2,7 @@ export function Header(props) {
   const { title } = props;
   return (
     <div className="header">
-      <h1>{title}</h1>
-      <hr />
+      <h2>{title}</h2>
     </div>
   );
 }
